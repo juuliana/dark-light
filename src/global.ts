167b5a1cedcd8dcb47d7c4df28529a289e560517
juuliana <jumaju10@hotmail.com>
2022-11-13 21:33:15 -0300

    feat: create global style

---
+++ src/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #261654;
+  }
+`;
