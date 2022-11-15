8a1a2bc95dc3ddc4f0e9637cdf9ac8609282e79d
juuliana <jumaju10@hotmail.com>
2022-11-15 19:43:57 -0300

    feat: create themes

---
+++ src/themes.ts
@@ -0,0 +1,19 @@
+export const dark = {
+  title: "dark",
+
+  colors: {
+    text: "#fff",
+    icon: "#fff",
+    background: "#261654",
+  },
+};
+
+export const light = {
+  title: "light",
+
+  colors: {
+    text: "#261654",
+    icon: "#261654",
+    background: "#fff",
+  },
+};
